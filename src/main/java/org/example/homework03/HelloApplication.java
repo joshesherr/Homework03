2d9ef1f1a8d126f115716534b4a9b76bc4e9d72d
colaola20 <soryo@farmingdale.edu>
2024-09-17 14:35:07 -0400

    Created a link between animation and a scene builder

--- src/main/java/org/example/homework03/HelloApplication.java
+++ src/main/java/org/example/homework03/HelloApplication.java
@@ -1,9 +1,19 @@
 package org.example.homework03;
 
+import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -11,9 +21,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toString());
 
+        //Image newRobot = new Image(getClass().getClassLoader().getResourceAsStream("robot.png"));
+        //ImageView newRobotImageView = new ImageView(newRobot);
+
+
+
+        //Scene scene = new Scene(new Group(newRobotImageView, path));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Robot");
         stage.setScene(scene);
         stage.show();
--- src/main/java/org/example/homework03/HelloController.java
+++ src/main/java/org/example/homework03/HelloController.java
@@ -1,9 +1,14 @@
 package org.example.homework03;
 
+import javafx.animation.PathTransition;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.util.Duration;
 
 public class HelloController {
 
@@ -18,5 +23,37 @@ public class HelloController {
 
     @FXML
     private VBox vBox;
+
+    @FXML
+    public void onMouseClick() {
+        //Setting up the path
+        Path path = new Path();
+        path.getElements().add (new MoveTo(537f, 228f));
+        path.getElements().add (new CubicCurveTo(240f, 230f, 500f, 340f, 600, 50f));
+
+        //Instantiating PathTransition class
+        javafx.animation.PathTransition pathTransition = new javafx.animation.PathTransition();
+
+        //Setting duration for the PathTransition
+        pathTransition.setDuration(Duration.millis(10000));
+
+        //Setting Node on which the path transition will be applied
+        pathTransition.setNode(robot);
+
+        //setting path for the path transition
+        pathTransition.setPath(path);
+
+        //setting orientation for the path transition
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+
+        //setting up the cycle count
+        pathTransition.setCycleCount(10);
+
+        //setting auto reverse to be true
+        pathTransition.setAutoReverse(true);
+
+        //Playing path transition
+        pathTransition.play();
+    }
 
 }
