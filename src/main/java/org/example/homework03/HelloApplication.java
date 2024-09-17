0f5e4d1b2c41f8c19a73a8e4a03fefbfa9bcfaed
colaola20 <soryo@farmingdale.edu>
2024-09-17 12:50:26 -0400

    added a background and a robot

--- src/main/java/org/example/homework03/HelloApplication.java
+++ src/main/java/org/example/homework03/HelloApplication.java
@@ -12,7 +12,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toString());
+
+        stage.setTitle("Robot");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/homework03/HelloController.java
+++ src/main/java/org/example/homework03/HelloController.java
@@ -1,14 +1,22 @@
 package org.example.homework03;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ImageView background;
+
+    @FXML
+    private ImageView robot;
+
+    @FXML
+    private VBox vBox;
+
 }
