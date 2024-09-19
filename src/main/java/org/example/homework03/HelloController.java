7f307274e73ea1e24c53b7d2bdd21ce33fa1d298
colaola20 <soryo@farmingdale.edu>
2024-09-19 15:28:15 -0400

    version1: the animation process for the robot

--- src/main/java/org/example/homework03/HelloController.java
+++ src/main/java/org/example/homework03/HelloController.java
@@ -1,6 +1,8 @@
 package org.example.homework03;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.image.Image;
@@ -81,43 +83,141 @@ public class HelloController {
     @FXML
     public void onStartAnimationClicked() {
 
-        //Instantiating PathTransition class
-        javafx.animation.PathTransition pathTransition = new javafx.animation.PathTransition();
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), event -> {
+            if (robot.getLayoutX() < maze.getImage().getWidth() / 2.0 - 8) {
+                if (isWallOnLeft()) {
+                    if (isWallInFront()) {
+                        robot.setRotate(robot.getRotate() + 90);
+                    } else {
+                        moveRobot(1, 0);
+                    }
+                } else {
+                    robot.setRotate(robot.getRotate() - 90);
+                    moveRobot(0, -1);
+                }
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
 
-        //Setting duration for the PathTransition
-        pathTransition.setDuration(Duration.millis(1000));
-
-        //Setting Node on which the path transition will be applied
-        pathTransition.setNode(robot);
-
-        //setting path for the path transition
-        pathTransition.setPath(findPath());
-
-        //setting orientation for the path transition
-        //pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-
-        //setting up the cycle count
-        pathTransition.setCycleCount(1);
-
-        //setting auto reverse to be true
-        pathTransition.setAutoReverse(true);
-
-        //Playing path transition
-        pathTransition.play();
+//        //Instantiating PathTransition class
+//        javafx.animation.PathTransition pathTransition = new javafx.animation.PathTransition();
+//
+//        //Setting duration for the PathTransition
+//        pathTransition.setDuration(Duration.millis(1000));
+//
+//        //Setting Node on which the path transition will be applied
+//        pathTransition.setNode(robot);
+//
+//        //setting path for the path transition
+//        pathTransition.setPath(findPath());
+//
+//        //setting orientation for the path transition
+//        //pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+//
+//        //setting up the cycle count
+//        pathTransition.setCycleCount(1);
+//
+//        //setting auto reverse to be true
+//        pathTransition.setAutoReverse(true);
+//
+//        //Playing path transition
+//        pathTransition.play();
     }
 
-    private Path findPath() {
-        //Setting up the path
-        Path path = new Path();
-        path.getElements().add (new MoveTo(0.0f, 0.0f));
+//    private Path findPath() {
+//        // Setting up the path
+//        Path path = new Path();
+//        robot.setLayoutX(0);
+//        robot.setLayoutY(0);
+//        path.getElements().add(new MoveTo(0.0f, (maze.getImage().getHeight() / 2.0)));
+//
+//        while (robot.getLayoutX() < maze.getImage().getWidth() - 8) {
+//            // Find the center of the robot image
+//
+//
+//            // Offset scan to center and outward into the movement direction
+////            double scanPosX = robot.getLayoutX() + roboCenterX + robotSpeed * roboCenterX;
+////            double scanPosY = robot.getLayoutY() + roboCenterY + 0 * roboCenterY;
+////
+////            // Search the image at the scan location for a color. Keep scan within bounds of image.
+////            Color colorAtPos = (scanPosX < maze.getImage().getWidth() && scanPosY < maze.getImage().getHeight()) ? maze.getImage().getPixelReader().getColor((int) (scanPosX), (int) (scanPosY)) : Color.BLACK;
+//
+//            // If the color is white or close to white, then proceed to move
+////            if (colorAtPos.getRed() > 0.9 && colorAtPos.getGreen() > 0.9 && colorAtPos.getBlue() > 0.9) {
+////                double newX = robot.getLayoutX() + robotSpeed;
+////                if (newX != robot.getLayoutX()) {
+////                    path.getElements().add(new LineTo(newX, robot.getLayoutY()));
+////                    robot.setLayoutX(newX);
+////                }
+////            } else {
+////                double newY = robot.getLayoutY() + robotSpeed;
+////                if (newY != robot.getLayoutY()) {
+////                    path.getElements().add(new LineTo(robot.getLayoutX(), newY));
+////                    robot.setLayoutY(newY);
+////                }
+////            }
+//        }
+//
+//        return path;
+//    }
+
+    private boolean isWallOnLeft() {
+        //test move robot to new position.
+        double newXPos = robot.getLayoutX();
+        double newYPos = robot.getLayoutY() - robotSpeed;
+
+        // find the center of the robot image
+        double roboCenterX = (robot.getImage().getWidth()/2.0);
+        double roboCenterY = (robot.getImage().getHeight()/2.0);
+
+        //keep movement within the bounds of the image.
+        if (newXPos>maze.getImage().getWidth()) newXPos = maze.getImage().getWidth();
+        else if (newXPos<0) newXPos = 0;
+        if (newYPos>maze.getImage().getHeight()) newYPos = maze.getImage().getHeight();
+        else if (newYPos<0) newYPos = 0;
+
+        //offset scan to center and outward into the movement direction
+        double scanPosX = newXPos + roboCenterX;
+        double scanPosY = newYPos + roboCenterY - roboCenterY;
 
 
-
-        path.getElements().add (new LineTo(100f, 0f));
-        path.getElements().add (new LineTo(-100f, 0f));
-        path.getElements().add (new LineTo(0f, -100f));
+        // Read the color at the scan position
+        Color colorAtPos = maze.getImage().getPixelReader().getColor((int) scanPosX, (int) scanPosY);
 
 
-        return path;
+        // Check if the color is not white (indicating a wall)
+        return !(colorAtPos.getRed() > 0.9 && colorAtPos.getGreen() > 0.9 && colorAtPos.getBlue() > 0.9);
+
     }
+
+    private boolean isWallInFront() {
+        //test move robot to new position.
+        double newXPos = robotSpeed + robot.getLayoutX();
+        double newYPos = robot.getLayoutY();
+
+        // find the center of the robot image
+        double roboCenterX = (robot.getImage().getWidth()/2.0);
+        double roboCenterY = (robot.getImage().getHeight()/2.0);
+
+        //keep movement within the bounds of the image.
+        if (newXPos>maze.getImage().getWidth()) newXPos = maze.getImage().getWidth();
+        else if (newXPos<0) newXPos = 0;
+        if (newYPos>maze.getImage().getHeight()) newYPos = maze.getImage().getHeight();
+        else if (newYPos<0) newYPos = 0;
+
+        //offset scan to center and outward into the movement direction
+        double scanPosX = newXPos + roboCenterX + roboCenterX;
+        double scanPosY = newYPos + roboCenterY;
+
+
+
+        // Read the color at the scan position
+        Color colorAtPos = maze.getImage().getPixelReader().getColor((int) scanPosX, (int) scanPosY);
+
+
+        // Check if the color is not white (indicating a wall)
+        return !(colorAtPos.getRed() > 0.9 && colorAtPos.getGreen() > 0.9 && colorAtPos.getBlue() > 0.9);
+    }
+
 }
